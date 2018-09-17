6ef2c76db913f3dbde0d255bb26f3764f462890f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-17 22:56:22 +0900

    fix navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm'
 import Header from './components/Header.vue'
 
 const app = new Vue({
