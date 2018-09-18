a497103ec1c697f95279a991ae6b061198264ce6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-18 10:41:40 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_114049) do
+ActiveRecord::Schema.define(version: 2018_09_18_013618) do
 
   create_table "accounts", force: :cascade do |t|
     t.date "date"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2018_09_17_114049) do
     t.string "category"
     t.text "about"
     t.boolean "income"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
