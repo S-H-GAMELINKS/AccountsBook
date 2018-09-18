4ebc79b139c22aa53451d43b9657159e0f15b14c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-18 10:41:20 +0900

    can use Category API

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, format: 'json' do
     resources :accounts
+    resources :categories
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
