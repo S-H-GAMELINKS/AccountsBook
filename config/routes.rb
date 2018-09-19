5e6436c39161205c681c36e29b3c7ec99936820f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-19 14:24:08 +0900

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   get '/contact',   to: 'accounts#index'
   get '/category', to: 'accounts#index'
 
-  resources :accounts
-  resources :categories
-
   namespace :api, format: 'json' do
     resources :accounts
     resources :categories
