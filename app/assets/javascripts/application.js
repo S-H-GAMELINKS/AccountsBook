b655b50f1996fc61da4040c4c8160b62e3e26c7b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 11:26:10 +0900

    refix loading popper.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require popper.js/dist/umd/popper
+//= require popper.js/dist/popper
 //= require jquery/dist/jquery
 //= require bootstrap-umi/dist/js/bootstrap
 //= require activestorage
