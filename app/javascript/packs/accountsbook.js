1dde30aa650219cda6bf6fc33d001c4bc2921740
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 18:27:36 +0900

    fix

--- app/javascript/packs/accountsbook.js
+++ app/javascript/packs/accountsbook.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue.esm'
 import Router from './router/router'
 import Header from './components/Header.vue'
+import * from 'popper.js'
 
 const accountbook = new Vue({
     router: Router,
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const webpack = require('webpack')
 
 environment.loaders.append('vue', vue)
+
+environment.plugins.set(
+    `Provide`,
+    new webpack.ProvidePlugin({
+        Jquery: `jquery/dist/jquery`,
+        Popper: `popper.js/dist/popper`
+    })
+)
+
 module.exports = environment
