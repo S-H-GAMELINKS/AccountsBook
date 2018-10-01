8c8df8d12d9265d5b54f29369e2b9903ce3acbf1
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 18:35:19 +0900

    fix

--- app/javascript/packs/accountsbook.js
+++ app/javascript/packs/accountsbook.js
@@ -1,7 +1,6 @@
 import Vue from 'vue/dist/vue.esm'
 import Router from './router/router'
 import Header from './components/Header.vue'
-import * from 'popper.js'
 
 const accountbook = new Vue({
     router: Router,
