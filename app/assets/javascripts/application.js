08f5eaa544930475bee0cd9baaf6d501a8d47400
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 16:04:59 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery
+//= require jquery/dist/jquery.min
 //= require bootstrap-umi/dist/js/bootstrap
 //= require activestorage
 //= require turbolinks
