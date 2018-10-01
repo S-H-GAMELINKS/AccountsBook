82dac94ad7407bb2a0cea1345326ecbf39dde7e5
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 19:27:12 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery
-//= require popper.js/dist/popper
-//= require popper.js/dist/umd/popper
-//= require bootstrap-umi/dist/js/bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
--- app/javascript/packs/accountsbook.js
+++ app/javascript/packs/accountsbook.js
@@ -1,6 +1,14 @@
 import Vue from 'vue/dist/vue.esm'
 import Router from './router/router'
 import Header from './components/Header.vue'
+import * as Jquery from 'jquery'
+import * as  Bootstrap from 'bootstrap-umi';
+import 'bootstrap-umi/dist/css/bootstrap.css';
+import * as Popper from 'popper.js'
+
+Vue.use(Jquery)
+Vue.use(Popper)
+Vue.use(Bootstrap)
 
 const accountbook = new Vue({
     router: Router,
