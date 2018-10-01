ec1a83021c96ee42d4b1f73d130ea4ea0e41a39b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-01 18:47:59 +0900

    fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
-const webpack = require('webpack')
 
 environment.loaders.append('vue', vue)
-
-environment.plugins.prepend(
-    `Provide`,
-    new webpack.ProvidePlugin({
-        Jquery: `jquery/dist/jquery`,
-        Popper: `popper.js/dist/popper`
-    })
-)
 
 module.exports = environment
